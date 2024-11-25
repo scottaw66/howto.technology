0c06ead21d8e6f41dae8e63bd193c0702e121d93
Scott Willsey <sunny.flame0794@fastmail.com>
2024-11-24 19:22:44 -0800

    Adds initial utilities

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import pagefind from "astro-pagefind";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import remarkToc from "remark-toc";
+import { remarkSocialLinks } from "./src/components/utilities/remark-social-links.mjs";
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  // Example: Change the themes
+  themes: ["material-theme-ocean", "light-plus", "github-dark-dimmed"],
+  themeCssSelector: (theme) => `[data-theme='${theme.name}']`,
+};
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://scottwillsey.com/",
+  integrations: [expressiveCode(astroExpressiveCodeOptions), pagefind()],
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: "contents" }], remarkSocialLinks],
+    rehypePlugins: [rehypeAccessibleEmojis],
+  },
+});
