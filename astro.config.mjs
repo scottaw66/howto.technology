dd5be464cbd7863767c227665be5bfea1feb3f47
Scott Willsey <sunny.flame0794@fastmail.com>
2024-12-11 23:44:57 -0800

    Customizes some CSS properties

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://howto.technology',
+  site: "https://howto.technology",
   integrations: [
     starlight({
       title: "HowTo.Technology",
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
     }),
   ],
 });
