6b5e07c08320000b2e87edb444d64f183a37069c
Scott Willsey <sunny.flame0794@fastmail.com>
2024-12-16 20:25:35 -0800

    Updates index page to show most recent docs

--- src/components.ts
+++ src/components.ts
@@ -1,2 +1,3 @@
 export { default as ArticleIndex } from "./components/ArticleIndex.astro";
+export { default as LatestGuides } from "./components/LatestGuides.astro";
 export { default as SubTopicIndex } from "./components/SubTopicIndex.astro";
