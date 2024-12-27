a773405269fce9651ce75c9320eec7bffb7bafc8
Scott Willsey <sunny.flame0794@fastmail.com>
2024-12-27 09:51:06 -0800

    Upgrades to Astro 5 and Starlight 0.3

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_2mqA34 from "../../assets/HowToTechnology.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/HowToTechnology.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_2mqA34]]);
+		
